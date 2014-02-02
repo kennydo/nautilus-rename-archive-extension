b630409f1dbe91606af3c2f699cd9f0b28ef0075
Kenny Do <kedo@ocf.berkeley.edu>
2014-02-02 04:08:24 -0800

    added a dialog before renaming files

--- rename_archive.py
+++ rename_archive.py
@@ -2,6 +2,7 @@ from gi.repository import Nautilus, GObject, Gtk
 import urllib
 import urlparse
 import zipfile
+import os
 import os.path
 
 
@@ -10,9 +11,18 @@ SUPPORTED_ZIP_MIME_TYPES = ['application/zip',
                             'application/zip-compressed']
 
 
-def get_filename(file_info):
+def get_file_path(file_info):
     uri = file_info.get_uri()
     return urllib.unquote(urlparse.urlparse(uri).path)
+
+
+def get_new_file_path(archive_path, directory_name):
+    if '.' in archive_path:
+        extension = archive_path.rsplit('.', 1)[1]
+        base_name = directory_name + '.' + extension
+    else:
+        base_name = directory_name
+    return os.path.join(os.path.dirname(archive_path), base_name)
 
 
 def get_zip_directory_names(filename):
@@ -22,14 +32,19 @@ def get_zip_directory_names(filename):
             names = [fname for fname in zip_file.namelist() if fname.endswith('/')]
     except zipfile.BadZipfile as e:
         pass
-    return [os.path.split(dir_name[:-1])[1] for dir_name in names]
+    return [os.path.basename(dir_name[:-1]) for dir_name in names]
+
 
 class RenameDialog(GObject.GObject):
-    def __init__(self, window):
-        self.dialog = Gtk.MessageDialog(window, 0, Gtk.MessageType.INFO,
-                                   Gtk.ButtonsType.OK, "This is an info dialog")
+    def __init__(self, window, original_name, new_name):
+        self.dialog = Gtk.MessageDialog(window, 0, Gtk.MessageType.QUESTION,
+                                   Gtk.ButtonsType.YES_NO, "Rename Archive")
+        self.dialog.format_secondary_text(
+            "Do you want to rename\n\"{0}\" to\n\"{1}\"".format(
+                original_name, new_name))
+
     def run(self):
-        self.dialog.run()
+        self.response = self.dialog.run()
 
     def destroy(self):
         self.dialog.destroy()
@@ -40,13 +55,23 @@ class RenameArchiveProvider(GObject.GObject, Nautilus.MenuProvider):
         pass
 
     def rename_directory_menuitem_cb(self, menu, cb_parameters):
-        fileinfo, window = cb_parameters
+        fileinfo, window, directory_name = cb_parameters
         if fileinfo.is_gone() or not fileinfo.can_write():
             return
+        old_path = get_file_path(fileinfo)
+        old_name = os.path.basename(old_path)
+        new_path = get_new_file_path(old_path, directory_name)
+        new_name = os.path.basename(new_path)
 
-        dialog = RenameDialog(window)
+        dialog = RenameDialog(window, old_name, new_name)
         dialog.run()
         dialog.destroy()
+
+        if dialog.response == Gtk.ResponseType.YES:
+            try:
+                os.rename(old_path, new_path)
+            except os.OSError as e:
+                print e
 
     def get_file_items(self, window, files):
         if len(files) != 1:
@@ -68,9 +93,9 @@ class RenameArchiveProvider(GObject.GObject, Nautilus.MenuProvider):
             top_menuitem.set_submenu(names_menu)
 
             # create the submenu items
-            filename = get_filename(selected_file)
+            file_path = get_file_path(selected_file)
 
-            directory_names = get_zip_directory_names(filename)
+            directory_names = get_zip_directory_names(file_path)
             if not directory_names:
                 no_directories_menuitem = Nautilus.MenuItem(name='RenameArchiveProvider::No Directories',
                                                             label='No directory names found',
@@ -84,7 +109,7 @@ class RenameArchiveProvider(GObject.GObject, Nautilus.MenuProvider):
                                                      tip='Rename to "' + directory_name + '"',
                                                      icon='')
                     dir_menuitem.connect('activate', self.rename_directory_menuitem_cb,
-                                         (selected_file, window))
+                                         (selected_file, window, directory_name))
                     names_menu.append_item(dir_menuitem)
 
             return [top_menuitem]
